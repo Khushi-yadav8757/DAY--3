b529ebb1456a07fd2fc5ba0e9d2cf4134d3df6f4
Khushi Yadav <kky957242@gmail.com>
2024-12-18 18:14:14 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,18 @@
+C++ Variables
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+string - stores text, such as "Hello World". String values are surrounded by double quotes
+bool - stores values with two states: true or false
+syntax for creating variable--
+type variableName = value;
+for example-- 
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum;
+  myNum = 15;
+  cout << myNum;
+  return 0;
+}
